b9d0c9ce89631f6c985ec4cd97ba6ebd47f08620
Joel Dierkes <joel@mixedbread.com>
2025-11-08 14:46:15 -0800

    chore: use the porper prod url

--- src/lib/mxbai.ts
+++ src/lib/mxbai.ts
@@ -3,7 +3,7 @@ import { isDevelopment } from "../utils";
 
 const BASE_URL = isDevelopment()
   ? "http://localhost:8000"
-  : "https://www.platform.mixedbread.com";
+  : "https://api.mixedbread.com";
 
 export function createMxbaiClient(authToken: string) {
   if (!authToken) {
